c461319982cbda403a70c823c44d60eb7ba804ac
LexaMedin <amedina.5523@unimar.edu.ve>
2026-04-09 23:00:28 -0400

    aded forms

---
+++ scrips.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#btnCorreo');
+btn.addEventListener('click', () => {
+    window.location.href = "mailto:aaaaa@gmail.com";
+});
+const instagram = document.querySelector('#ins');
+instagram.addEventListener('click', () => {
+    window.location.href = "https://www.instagram.com/";
+});
+const whats = document.querySelector('#wame');
+whats.addEventListener('click',()=>{
+  window.location.href=" https://wa.me/12345345"
+});
